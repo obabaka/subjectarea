f05911aea9aae61eb9c86c7c71f60a15befba4ed
obabaka <123298875+obabaka@users.noreply.github.com>
2023-04-08 00:50:16 +1000

    Update subject area.cpp

--- subject area.cpp
+++ subject area.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-#include "Header.h"
-#include <string>
-#include <ctime>
+#include<string>
+#include "header.h"
+
 
 int main()
 {
-    setlocale(LC_ALL, "RUSSIAN");
-    avto car, car1;
-    car.company = "Nissan";
-    car.name = "Skyline";
-    car.year = 1999;
-    car.speed = 325;
-    std::cout << "Первый автомобиль:\n" << car.company << " " << car.name << "\n" << car.year << " года\n"<< car.speed <<" л.с.\n" <<  std::endl;
-    car1.company = "Toyota";
-    car1.name = "Mark II";
-    car1.year = 1996;
-    car1.speed = 290;
-    std::cout << "Второй автомобиль:\n" << car1.company << " " << car1.name << "\n" << car1.year << " года\n" << car1.speed << " л.с.\n" << std::endl;
-    std::cout << race(car, car1);
- 
+	person first;
+
+	first.name = "Alan";
+	first.number = 5;
+	first.specialization = "biology";
+	first.complited_work = false;
+
+
+	change_level(first);
+	first.promotion("head of labaratory");
+	first.number < 10 ? std::cout << "error\n" : first.number == 10 ? std::cout << "director's access\n" : std::cout << "access allowed\n";
+
+
+	person second;
+	second = person();
 }
